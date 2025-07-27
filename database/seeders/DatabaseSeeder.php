4070ad60e506feccc4ea2fb64bbea84b95a91853
keval5252 <kevalchokhaliya@gmail.com>
2025-07-27 13:03:47 +0530

    chnages

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(defaultAdmin::class);
-        $this->call(CountriesSeeder::class);
         $this->call(PermissionSeeder::class);
     }
 }
